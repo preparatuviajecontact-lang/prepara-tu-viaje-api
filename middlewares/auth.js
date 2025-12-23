96e35ce5eb11db75f460779a99226b8b41e51776
preparatuviajecontact-lang <preparatuviajecontact@gmail.com>
2025-12-23 15:20:24 -0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,23 +1,33 @@
 import { verifyApiKey } from '../utils/verifyApiKey.js';
 
 export const authApiKey = async (req, res, next) => {
-  const apiKey = req.headers["x-api-key"];
+    const apiKey = req.headers["x-api-key"];
+    const { companyId } = req.params;
 
-  if (!apiKey) {
-    return res.status(401).json({
-      success: false,
-      error: "API key requerida"
-    });
-  }
+    if (!apiKey) {
+        return res.status(401).json({
+            success: false,
+            error: "API key requerida"
+        });
+    }
 
-  const isValid = await verifyApiKey(apiKey);
-  if (!isValid.valid) {
-    return res.status(401).json({
-      success: false,
-      error: isValid.reason
-    });
-  }
+    if (!companyId) {
+        return res.status(400).json({
+            success: false,
+            error: "companyId requerido en la ruta"
+        });
+    }
 
-  req.apiKey = apiKey;
-  next();
+    const isValid = await verifyApiKey(apiKey, companyId);
+
+    if (!isValid.valid) {
+        return res.status(401).json({
+            success: false,
+            error: isValid.reason
+        });
+    }
+
+    req.apiKey = apiKey;
+    req.companyId = companyId;
+    next();
 };
